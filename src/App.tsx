a785f2e9f59f44f791dd1dc1fa010967de2874a9
Ghansham Pawar <ghansham.pawar.official@gmail.com>
2025-09-07 21:45:22 +0530

    Front end basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstNo, setFirstNo] = useState(0);
+  const [secondNo, setSecondNo] = useState(0);
+
+  const trackChange1 = (e: any) => {
+    e.preventDefault();
+
+    console.log("Hi Hi", e);
+    setFirstNo(e.target.value);
+  };
+
+  const trackChange2 = (e: any) => {
+    e.preventDefault();
+
+    console.log("Hi Hi", e);
+    setSecondNo(e.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div
+        style={{
+          border: "1px solid red",
+          width: 700,
+          height: 700,
+          backgroundColor: "beige",
+        }}
+      >
+        <div
+          style={{
+            border: "1px solid black",
+            color: "red",
+            fontSize: "20px",
+          }}
+        >
+          Enter two numbers :
+        </div>
+
+        <label style={{ color: "black" }}>
+          Enter value 1:{" "}
+          <input
+            style={{
+              backgroundColor: "white",
+              color: "black",
+            }}
+            onChange={(e) => trackChange1(e)}
+            name="firstNo"
+          />
+        </label>
+
+        <hr />
+
+        <label style={{ color: "black" }}>
+          Enter value 2:{" "}
+          <input
+            style={{
+              backgroundColor: "white",
+              color: "black",
+            }}
+            onChange={(e) => trackChange2(e)}
+            name="secondNo"
+          />
+        </label>
+
+        <hr />
+
+        <button
+          style={{ color: "green" }}
+          onClick={(e) => console.log("onClick")}
+        >
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <hr />
+
+        <div style={{ color: "black" }}>
+          Your entered values are:
+          <hr />
+          value 1: {firstNo}
+          <hr />
+          value 2: {secondNo}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
